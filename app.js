18dc322eed2e410fba97e62c990b44d3e7816284
Dmitriy Kotulsky <66743608+DimRom76@users.noreply.github.com>
2020-10-10 19:24:19 +0300

    class

---
+++ app.js
@@ -0,0 +1,59 @@
+//     КЛАССЫ В JS
+
+class Car {
+  // Write code under this line
+  static getSpecs(car) {
+    return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car._price}`;
+  }
+  constructor(settings) {
+    const { maxSpeed, price, speed = 0, isOn = false, distance = 0 } = settings;
+
+    this.speed = speed;
+    this._price = price;
+    this.maxSpeed = maxSpeed;
+    this.isOn = isOn;
+    this.distance = distance;
+  }
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+  }
+  accelerate(value) {
+    this.speed += value;
+    this.speed = this.speed > this.maxSpeed ? this.maxSpeed : this.speed;
+  }
+  decelerate(value) {
+    this.speed -= value;
+    this.speed = this.speed < 0 ? 0 : this.speed;
+  }
+  drive(hours) {
+    this.distance += this.isOn ? hours * this.speed : 0;
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+// console.log(Car.getSpecs(mustang));
+// 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
+
+// console.log(mustang.price); // 2000
+mustang.price = 4000;
+// console.log(mustang.price); // 4000
